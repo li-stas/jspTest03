0d2b637e03e486edbd53b105abc5c4da537e2e23
lista <lista@bk.ru>
2020-03-05 22:32:48 +0200

    умеет хранить и добавлять пользователей, а  также выводить список их имен.

--- src/main/java/app/servlets/AddServlet.java
+++ src/main/java/app/servlets/AddServlet.java
@@ -14,8 +14,32 @@ import java.io.PrintWriter;
  * будет обрабатывать запросы, поступившие по адресу /add;
  */
 public class AddServlet extends HttpServlet {
-
+    /*
+    AddServlet, чтобы выводилось уведомление об успешном добавлении пользователя. Для этого в методе doPost()
+    после того, как добавили нового пользователя в модель, можем добавить имя этого пользователя в
+    атрибуты объекта req и передать управление обратно во вьюху add.jsp. А в ней уже сделать участок с
+    Java-кодом, в котором происходит проверка, есть ли такой атрибут в запросе, и если да — то вывод сообщения
+    о том, что пользователь успешно добавлен.
+     */
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/add.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String password = req.getParameter("pass");
+        User user = new User(name, password);
+        Model model = Model.getInstance();
+        model.add(user);
+
+        req.setAttribute("userName", name);
+        doGet(req, resp);
+    }
+
+    protected void doGet03(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         /*
         вытащим из запроса параметры name и pass, которые отправила форма (если вы их в форме назвали по-другому
         — тогда именно те названия и пишете). После этого создадим объект нашего пользователя,
--- web/views/add.jsp
+++ web/views/add.jsp
@@ -10,20 +10,40 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-<head>
-    <title>Add new user</title>
-</head>
-<body>
-      <%--page "Add new user"--%>
-      <form method="post">
-          <label>Name:
-              <input type="text" name="name"><br />
-          </label>
+    <head>
+        <title>Add new user</title>
+    </head>
 
-          <label>Password:
-              <input type="password" name="pass"><br />
-          </label>
-          <button type="submit">Submit</button>
-      </form>
-</body>
+    <body>
+        <div>
+            <h1>Super app!</h1>
+        </div>
+
+        <div>
+            <%
+                if (request.getAttribute("userName") != null) {
+                    out.println("<p>User '" + request.getAttribute("userName") + "' added!</p>");
+                }
+            %>
+            <div>
+                <div>
+                    <h2>Add user</h2>
+                </div>
+
+                <form method="post">
+                    <label>Name:
+                        <input type="text" name="name"><br/>
+                    </label>
+                    <label>Password:
+                        <input type="password" name="pass"><br/>
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+
+        <div>
+            <button onclick="location.href='/'">Back to main</button>
+        </div>
+    </body>
 </html>
