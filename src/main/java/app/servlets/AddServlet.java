573258b4e6c5c3708de6f7913829a12b95252016
lista <lista@bk.ru>
2020-03-05 22:55:43 +0200

    кирилица

--- src/main/java/app/servlets/AddServlet.java
+++ src/main/java/app/servlets/AddServlet.java
@@ -23,12 +23,14 @@ public class AddServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/add.jsp");
         requestDispatcher.forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
         String password = req.getParameter("pass");
         User user = new User(name, password);
